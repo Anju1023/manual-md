f12fe56ed6c59fd419f2dc425d74dbaf26c378eb
Anju <niafuguapricottaro@gmail.com>
2025-11-28 17:05:56 +0900

    不要なファイルの削除

--- .next/dev/server/chunks/ssr/_next-internal_server_app_02_weekly_order_supplies_page_actions_4bfc34fe.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/02_weekly/order_supplies/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_02_weekly_order_supplies_page_actions_4bfc34fe.js.map
--- .next/dev/server/chunks/ssr/_next-internal_server_app_02_weekly_page_actions_415ed708.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/02_weekly/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_02_weekly_page_actions_415ed708.js.map
